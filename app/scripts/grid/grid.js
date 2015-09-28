c6655c18084089b94f3db2c1dae996645d207f70
MichalObi <ndwz123@gmail.com>
2015-09-28 23:07:52 +0200

    check test for grid tiles

--- app/scripts/grid/grid.js
+++ app/scripts/grid/grid.js
@@ -28,8 +28,56 @@ angular.module('Grid', [])
 	return Tile;
 })
 .service('GridService', function(TileModel){
+
 	this.tiles = [];
 	this.tiles.push(new TileModel({x: 1, y: 1}, 2));
 	this.tiles.push(new TileModel({x: 1, y: 2}, 2));
 
+	this.buildEmptyGameBoard = function() {
+	    var self = this;
+	    // Initialize our grid
+	    for (var x = 0; x < service.size * service.size; x++) {
+	      this.grid[x] = null;
+	    }
+
+	    // Initialize our tile array
+	    // with a bunch of null objects
+	    this.forEach(function(x,y) {
+	      self.setCellAt({x:x,y:y}, null);
+	    });
+	  }; 
+	  
+	this.forEach = function(cb) {
+	  var totalSize = this.size * this.size;
+	  for (var i = 0; i < totalSize; i++) {
+	    var pos = this._positionToCoordinates(i);
+	    cb(pos.x, pos.y, this.tiles[i]);
+	  }
+	};
+
+	// Set a cell at position
+	this.setCellAt = function(pos, tile) {
+	  if (this.withinGrid(pos)) {
+	    var xPos = this._coordinatesToPosition(pos);
+	    this.tiles[xPos] = tile;
+	  }
+	};
+
+	// Fetch a cell at a given position
+	this.getCellAt = function(pos) {
+	  if (this.withinGrid(pos)) {
+	    var x = this._coordinatesToPosition(pos);
+	    return this.tiles[x];
+	  } else {
+	    return null;
+	  }
+	};
+
+	// A small helper function to determine if a position is
+	// within the boundaries of our grid
+	this.withinGrid = function(cell) {
+	  return cell.x >= 0 && cell.x < this.size &&
+	          cell.y >= 0 && cell.y < this.size;
+	}; 
+	
 });
--- test/unit/grid_spec.js
+++ test/unit/grid_spec.js
@@ -28,4 +28,48 @@ describe('Grid', function() {
 
     });
   });
+
+  describe('GridService', function() {
+
+    var gridService, tileModel;
+
+    // inject gridService and tileModel (object) to make some sweet test
+
+    beforeEach(inject(function(GridService, TileModel) {
+      gridService = GridService;
+      tileModel = TileModel;
+    }));
+
+    describe('.buildEmptyGameBoard', function() {
+      var nullArr;
+      
+      beforeEach(function() {
+        nullArr = [];
+        for (var x = 0; x < 16; x++) {
+          nullArr.push(null);
+        }
+      });
+
+      it('should clear out the grid array with nulls', function() {
+        var grid = [];
+        for (var x = 0; x < 16; x++) {
+          grid.push(x);
+        }
+        gridService.grid = grid;
+        gridService.buildEmptyGameBoard();
+        expect(gridService.grid).toEqual(nullArr);
+      });
+    
+    //   it('should clear out the tiles array with nulls', function() {
+    //     var tiles = [];
+    //     for (var x = 0; x < 16; x++) {
+    //       tiles.push(x);
+    //     }
+    //     gridService.tiles = tiles;
+    //     gridService.buildEmptyGameBoard();
+    //     expect(gridService.tiles).toEqual(nullArr);
+    //   });
+    });
+
+  });
 });
